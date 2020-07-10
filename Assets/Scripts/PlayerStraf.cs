38074be8c9246f02ee7653b4ff6af20e29afaf7d
feltos <guillaumevolgyesi@gmail.com>
2020-07-10 06:02:58 +0200

    add test scene for straf

---
+++ Assets/Scripts/PlayerStraf.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class PlayerStraf : MonoBehaviour
+{
+    [SerializeField]Transform posA;
+    [SerializeField]Transform posB;
+    [SerializeField]Transform posC;
+
+    List<Transform> allPos = new List<Transform>();
+
+    Transform newPoint;
+    Transform oldPoint;
+    float speed = 1;
+
+    bool atDestination = false;
+    void Start()
+    {
+        newPoint = posB;
+        allPos.Add(posA);
+        allPos.Add(posB);
+        allPos.Add(posC);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, newPoint.position, Time.deltaTime * speed);
+
+        if (Vector3.Distance(transform.localPosition, newPoint.position) < 0.1f)
+        {
+            var index = Random.Range(0, allPos.Count);
+            newPoint = allPos[index];
+            Debug.Log(newPoint);
+            speed = Random.Range(1, 3);
+        }
+    }
+}
